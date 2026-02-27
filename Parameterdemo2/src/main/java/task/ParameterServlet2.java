fc35bc313e03e4ba53019dcf01404c6b4385545f
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-02-27 10:51:22 +0530

    Get & Post File Handling Task

---
+++ Parameterdemo2/src/main/java/task/ParameterServlet2.java
@@ -0,0 +1,65 @@
+package task;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ParameterServlet2")
+public class ParameterServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ParameterServlet2() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		String name = request.getParameter("userName");
+		String age= request.getParameter("urAge");
+		String education = request.getParameter("urEdu");
+		String city = request.getParameter("urCity");
+		
+		pw.print("<html><body>");
+		pw.println("<h2>Using doGet</h2>");
+		pw.print("<h2>Welcome "+name+"</h2>");
+		pw.print("<h4>Your Age : "+age+" </h4>");
+		pw.print("<h4>Your Education: "+education+" </h4>");
+		pw.print("<h4>Your City: "+city+" </h4>");
+		pw.print("</body></html>");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+	//	doGet(request, response);
+		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		String name = request.getParameter("userName");
+		String age = request.getParameter("urAge");
+		String education = request.getParameter("urEdu");
+		String city = request.getParameter("urCity");
+		String pass = request.getParameter("urPassword");
+		
+		pw.print("<html><body>");
+		pw.println("<h2>Using doPost</h2>");
+		pw.print("<h2>Welcome "+name+"</h2>");
+		pw.print("<h4>Your Age: "+age+" </h4>");
+		pw.print("<h4>Your Education: "+education+" </h4>");
+		pw.print("<h4>Your City: "+city+" </h4>");
+		pw.print("<h4>Your Password is:  "+pass+"</h4>");
+		pw.print("</body></html>");
+		
+	}
+
+}
