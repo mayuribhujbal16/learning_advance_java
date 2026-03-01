166cfbcfb05f0bda399463930784130cab07feea
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-03-01 21:46:55 +0530

    HTTPS Session

---
+++ DemoHTTPSession/src/main/java/com/LoginServlet.java
@@ -0,0 +1,50 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{
+			response.setContentType("text/html");
+			PrintWriter pw = response.getWriter();
+			
+			String nm = request.getParameter("userName");
+			pw.print("Welcome "+nm);
+			
+			HttpSession hs= request.getSession();
+			hs.setAttribute("sName", nm);
+			
+			pw.print("<a href='Servlet2'>visit</a>");
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ DemoHTTPSession/src/main/java/com/Servlet2.java
@@ -0,0 +1,60 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		try 
+		{
+			response.setContentType("text/html");
+			PrintWriter pw = response.getWriter();
+			
+			HttpSession hs = request.getSession();
+			
+			String n = (String)hs.getAttribute("sName");
+			pw.print("Hello "+n);
+			pw.print("Website under development, coming soon....Thanks For Visiting ");
+			
+			pw.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
