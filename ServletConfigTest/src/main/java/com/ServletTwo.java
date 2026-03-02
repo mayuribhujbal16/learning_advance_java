1ad8056e5accff0af41a52d4d8df17e3eae7083b
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-03-02 20:28:51 +0530

    Servlet Configuration

--- ServletConfigTest/src/main/java/com/ServletTwo.java
+++ ServletConfigTest/src/main/java/com/ServletTwo.java
@@ -10,17 +10,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class ServletConfigClass
+ * Servlet implementation class ServletTwo
  */
-// @WebServlet("/ServletConfigClass")
-
-public class ServletConfigClass extends HttpServlet {
+// @WebServlet("/ServletTwo")
+public class ServletTwo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletConfigClass() {
+    public ServletTwo() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,16 +29,27 @@ public class ServletConfigClass extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+
 		response.setContentType("text/hmtl");
 		PrintWriter pw = response.getWriter();	
 		ServletConfig sc = getServletConfig();
 		
-		String n = sc.getInitParameter("Name");
-		String s = sc.getInitParameter("Surname");
+		String n = sc.getInitParameter("Driver");
+		String s = sc.getInitParameter("Setting");
 		
 		pw.print("Your init Parameter is: "+n+" "+s);
 		pw.close();
 		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
