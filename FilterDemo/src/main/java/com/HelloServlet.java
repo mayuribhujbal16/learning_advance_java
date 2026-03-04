e400a8a38c4dc8771854a810498de8e3cc169803
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-03-04 18:31:44 +0530

    Filter Process

---
+++ FilterDemo/src/main/java/com/HelloServlet.java
@@ -0,0 +1,45 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+//@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.setContentType("text/html");
+		
+		PrintWriter pw = response.getWriter();
+		
+		pw.print("Welcome to filter servlet");
+		
+		
+	}
+
+	private PrintWriter getWriter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
